0ccd2449db1640e8a5654d68e5c60d0da4fe5bc8
Sotaur <christianmarklambert@gmail.com>
2018-12-10 16:29:34 -0700

    Updated for current usage

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+require('dotenv').config();
 module.exports = {
 
     development: {
@@ -7,34 +7,18 @@ module.exports = {
         connection: {
             host: '127.0.0.1',
             user: 'root',
-            password: '',
+            password: process.env.DB_PASSWORD,
             database: 'home_data_test',
             charset: 'utf8'
-        }
-    },
-
-    staging: {
-        client: 'mysql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
         }
     },
 
     production: {
         client: 'mysql',
         connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
+            database: 'home_data',
+            unixSocket: 'var/lib/mysql/mysql.sock',
+            user: 'pi'
         },
         pool: {
             min: 2,
