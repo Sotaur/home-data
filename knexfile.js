0963671484fb5a3a5de487f0660f40e523f4aa21
Sotaur <christianmarklambert@gmail.com>
2018-12-10 09:20:59 -0700

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+
+    development: {
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'home_data_test',
+            charset: 'utf8'
+        }
+    },
+
+    staging: {
+        client: 'mysql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    },
+
+    production: {
+        client: 'mysql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    }
+
+};
